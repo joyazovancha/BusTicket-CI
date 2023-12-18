deb5dfe6bdd7dc70279ffb4b6dcdfbd28623b2f9
jerichols <145383781+joyazovancha@users.noreply.github.com>
2023-12-18 10:34:36 +0700

    Initial commit

---
+++ application/config/recaptcha.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$config['recaptcha_site_key'] = '6LdI-JkUAAAAAJVPdj9ULcQNvVZalcUciPg7XghT';
+$config['recaptcha_secret_key'] = '6LdI-JkUAAAAAEa1uhFPw_km9nvZOM3ycclpsLtN';
+
+
+$config['recaptcha_lang'] = 'en';
+
